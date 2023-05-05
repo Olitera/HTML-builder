03999905b2e17da18b19e9be6c8c43512ac59c59
Olitera <lu_gu@mail.ru>
2023-05-05 10:13:57 +0200

    feat: do 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const readStream = fs.createReadStream(__dirname+'/text.txt');
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'));
 let all = '';
 readStream.on('data', chunk => all += chunk);
 readStream.on('end', () => console.log(all));
