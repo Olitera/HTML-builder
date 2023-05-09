747322752391de3eca511fbc801e2666c4f42542
Olitera <lu_gu@mail.ru>
2023-05-09 08:33:46 +0200

    fix: fix name style.file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -13,7 +13,7 @@ let count = 0;
 let writeStyles;
 
 fs.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, () => {
-  writeStyles = fs.createWriteStream(path.join(__dirname, 'project-dist', 'styles.css'));
+  writeStyles = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
 });
 
 readStream = fs.createReadStream(path.join(__dirname, 'template.html'));
@@ -33,11 +33,9 @@ fs.readdir(path.join(__dirname, 'components'), (err, files) => {
           readHtml.on('data', chunk => {
             sum++;
             Html = Html.replace(files[i].padStart(files[i].length + 2, '{{').padEnd(files[i].length + 4, '}}').slice(0, files[i].length-3)+'}}', chunk);
-            // console.log(files[i].padStart(files[i].length + 2, '{{').padEnd(files[i].length + 4).slice(0, files[i].length-3)+'}}');
             if (htmlCount === sum) {
               writeHTML = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
               writeHTML.write(Html);
-              // console.log('bvhh')
             }
           });
         } else {
@@ -73,7 +71,6 @@ function copy(dirName, assetsDir) {
   fsPromises.mkdir(dirName, { recursive: true })
     .then(() => fsPromises.readdir(assetsDir))
     .then((filesArray) => {
-      // console.log(filesArray);
       for (const file of filesArray) {
         fs.stat(path.join(assetsDir, file), (err, stats) => {
           if (err) throw err;
